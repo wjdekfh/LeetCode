8510510732144b83d51f2f06633831ee36b14a4b
Jeong <42461643+wjdekfh@users.noreply.github.com>
2022-08-28 01:09:34 +0900

    Time: 18 ms, Memory: 45.9 MB - LeetHub

--- third-maximum-number/third-maximum-number.java
+++ third-maximum-number/third-maximum-number.java
@@ -5,13 +5,9 @@ class Solution {
                 .boxed()
                 .sorted(Collections.reverseOrder())
                 .limit(3)
-                .sorted()
                 .collect(Collectors.toList());
 
-        if(list.size() == 3) {
-            return list.get(0);
-        }else {
-            return list.get(list.size()-1);
-        }
+
+        return list.get(list.size() == 3 ? 2:0);
     }
 }
