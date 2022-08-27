87155ce3e8bbdf34a1ba5d493dd11c6f6bca8986
Jeong <42461643+wjdekfh@users.noreply.github.com>
2022-08-27 21:20:24 +0900

    Time: 1 ms, Memory: 43.5 MB - LeetHub

---
+++ merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int nums1Idx = 0;
+        int nums2Idx = 0;
+        Queue<Integer> queue = new LinkedList();
+        for(int i=0; i<nums1.length; i++) {
+            int value = nums1[i];
+            
+            if(value == 0 && nums1Idx >= m) {
+                value = 999;
+            }
+            
+            queue.add(value);
+            nums1Idx++;
+            
+            if(nums2Idx < n) {
+                if(queue.peek() > nums2[nums2Idx]) {
+                    nums1[i] = nums2[nums2Idx++];
+                    continue;
+                }
+            }
+
+            nums1[i] = queue.poll();
+        }
+    }
+}
