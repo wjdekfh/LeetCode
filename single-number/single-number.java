a23fddd211c9fdbbef0c23cc63a3dafa7a098bc9
Jeong <42461643+wjdekfh@users.noreply.github.com>
2022-09-13 15:27:10 +0900

    Time: 26 ms, Memory: 53.5 MB - LeetHub

---
+++ single-number/single-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int value = 0;
+        Set<Integer> set = new HashSet();
+        
+        for(int i=0; i<nums.length; i++) {
+            if (set.contains(nums[i])) {
+                set.remove(nums[i]);
+            }else {
+                set.add(nums[i]);   
+            }
+        }
+        
+        Iterator<Integer> iter = set.iterator();
+        while(iter.hasNext()) {
+            value = iter.next();
+        }
+        
+        return value;
+    }
+}
