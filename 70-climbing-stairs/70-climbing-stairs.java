575f087a1557683a18d672b1121e3ab12a10556e
Jeong <42461643+wjdekfh@users.noreply.github.com>
2022-09-22 00:39:33 +0900

    Time: 0 ms (100.00%), Space: 40.8 MB (49.62%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -4,6 +4,7 @@ class Solution {
         dp[0] = 1;
         dp[1] = 1;
 
+        // 피보나치 처럼 경우의 수 구해서 리턴
         for(int i=2; i<dp.length; i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
